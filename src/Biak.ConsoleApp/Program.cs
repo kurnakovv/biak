8ccae15f1b0e18e8cfd7b8e75c3080b2ba85b5f1
kurnakovv <maksk210@gmail.com>
2026-02-08 15:14:39 +0900

    Add deploy-nuget-org.yml (#2)

--- src/Biak.ConsoleApp/Program.cs
+++ src/Biak.ConsoleApp/Program.cs
@@ -2,4 +2,4 @@
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for full license information.
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello from biak!");
