e621bc7fac5a0c83a8572900e92fb7dae167e59e
kurnakovv <maksk210@gmail.com>
2026-02-12 16:12:20 +0900

    Add dotnet biak setup command (#10)

---
+++ src/Biak.ConsoleApp/Constants/CommandArgumentConstant.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2026 kurnakovv
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for full license information.
+
+namespace Biak.ConsoleApp.Constants;
+
+/// <summary>
+/// All consts command argumets.
+/// </summary>
+public class CommandArgumentConstant
+{
+    /// <summary>
+    /// Setup biak env (create .biak folder with .editorconfig-main file).
+    /// </summary>
+    public const string SETUP = "setup";
+}
