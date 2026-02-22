bee2b1d873fc3b57c67e764c96990a216939fd5e
kurnakovv <maksk210@gmail.com>
2026-02-22 10:27:46 +0900

    Add dotnet biak enable command (#23)

---
+++ src/Biak.ConsoleApp/Helpers/SetupHelper.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) 2026 kurnakovv
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for full license information.
+
+using Biak.ConsoleApp.Constants;
+using Biak.ConsoleApp.Models;
+
+namespace Biak.ConsoleApp.Helpers;
+
+/// <summary>
+/// Provides helper methods for setup-related operations, such as locating configuration files required by the
+/// application.
+/// </summary>
+public static class SetupHelper
+{
+    /// <summary>
+    /// Get .editorconfig paths.
+    /// </summary>
+    /// <returns>.editorconfig paths.</returns>
+    public static EditorconfigPaths GetEditorconfigPaths()
+    {
+        EditorconfigPaths result = new();
+
+        string currentDirectory = Directory.GetCurrentDirectory();
+
+        string editorconfigMainPath = Path.Join(currentDirectory, ".biak", ".editorconfig-main");
+
+        if (!File.Exists(editorconfigMainPath))
+        {
+            Console.WriteLine(UIConstant.BIAK_NOT_INITIALIZED);
+            Console.WriteLine(UIConstant.RUN_BIAK_SETUP);
+            return result;
+        }
+        result.MainValue = editorconfigMainPath;
+
+        string editorConfigPath = Path.Join(currentDirectory, ".editorconfig");
+
+        if (!File.Exists(editorConfigPath))
+        {
+            Console.WriteLine(UIConstant.EDITORCONFIG_NOT_FOUND + editorConfigPath);
+            return result;
+        }
+        result.Value = editorConfigPath;
+
+        return result;
+    }
+}
---
+++ tests/Biak.ConsoleApp.UnitTests/Commands/EnableCommandTests.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2026 kurnakovv
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for full license information.
+
+using Biak.ConsoleApp.Commands;
+using Biak.ConsoleApp.Constants;
+
+namespace Biak.ConsoleApp.UnitTests.Commands;
+
+public class EnableCommandTests
+{
+    [Fact]
+    public void IsRunnableFalseForEmptyParams()
+    {
+        Assert.False(EnableCommand.IsRunnable([]), "You can't run enable command without `enable` argument");
+    }
+
+    [Fact]
+    public void IsRunnableFalseForInvalidParams()
+    {
+        Assert.False(EnableCommand.IsRunnable(["blabla"]), "`blabla` is invalid param");
+        Assert.False(EnableCommand.IsRunnable([CommandArgumentConstant.ENABLE, "invalid"]), "`enable` + `invalid` is invalid");
+        Assert.False(EnableCommand.IsRunnable(["invalid", CommandArgumentConstant.ENABLE]), "`invalid` + `enable` is invalid");
+        Assert.False(EnableCommand.IsRunnable([CommandArgumentConstant.SETUP]), "`setup` is invalid");
+    }
+
+    [Fact]
+    public void IsRunnableTrueForSingleDisableParam()
+    {
+        Assert.True(EnableCommand.IsRunnable([CommandArgumentConstant.ENABLE]));
+    }
+}
