ba8943cf4700ea7e96da5db2f0a6ba96534da80a
kurnakovv <maksk210@gmail.com>
2026-02-27 14:04:05 +0900

    Add dotnet biak --help command (#34)

--- src/Biak.ConsoleApp/Program.cs
+++ src/Biak.ConsoleApp/Program.cs
@@ -23,6 +23,10 @@ public static class Program
         {
             Console.WriteLine(DocsConstant.GREETING);
         }
+        else if (args.Length == 1 && args.Single() == CommandArgumentConstant.HELP)
+        {
+            Console.WriteLine(DocsConstant.HELP);
+        }
         else if (SetupCommand.IsRunnable(args))
         {
             await SetupCommand.RunAsync();
