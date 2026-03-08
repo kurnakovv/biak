209c8fc9a3bfa8bb6bd82af1d0a09a032c93a277
kurnakovv <maksk210@gmail.com>
2026-03-08 15:39:50 +0900

    Add severityWhenDisabled config (#43)

---
+++ src/Biak.ConsoleApp/Models/BiakConfig.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2026 kurnakovv
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for full license information.
+
+using System.Text.Json.Serialization;
+using Biak.ConsoleApp.Enums;
+
+namespace Biak.ConsoleApp.Models;
+
+/// <summary>
+/// .biak/config.json model.
+/// </summary>
+public class BiakConfig
+{
+    /// <summary>
+    /// Severity level when `dotnet biak disable`.
+    /// </summary>
+    [JsonPropertyName("severityWhenDisabled")]
+    public SeverityLevelType SeverityWhenDisabled { get; set; } = SeverityLevelType.None;
+}
