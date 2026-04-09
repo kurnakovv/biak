030a003c3361365b8b1949895e25862ad47acf49
kurnakovv <maksk210@gmail.com>
2026-04-09 17:36:40 +0900

    Add private network protection (#64)

--- src/Biak.ConsoleApp/Helpers/ImportHelper.cs
+++ src/Biak.ConsoleApp/Helpers/ImportHelper.cs
@@ -2,6 +2,7 @@
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for full license information.
 
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using Biak.ConsoleApp.Constants;
@@ -72,9 +73,14 @@ public static class ImportHelper
 
     private static async Task<string?> ResolveImportAsync(string value, string biakFullPath, string newline)
     {
-        if (Uri.TryCreate(value, UriKind.Absolute, out Uri? uri) &&
-            (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+        if (Uri.TryCreate(value, UriKind.Absolute, out Uri? uri))
         {
+            if (uri.Scheme != Uri.UriSchemeHttps || !await IsSafeUriAsync(uri))
+            {
+                Console.WriteLine($"{ImportConstant.INVALID_URL_FORMAT} {value}");
+                return null;
+            }
+
             try
             {
                 using HttpResponseMessage response = await s_httpClient.GetAsync(uri);
@@ -120,5 +126,68 @@ public static class ImportHelper
             .Replace("\r\n", "\n", StringComparison.Ordinal)
             .Replace("\r", "\n", StringComparison.Ordinal)
             .Replace("\n", newline, StringComparison.Ordinal);
+    }
+
+    private static async Task<bool> IsSafeUriAsync(Uri uri)
+    {
+        try
+        {
+            string host = uri.Host;
+
+            if (host.Equals("localhost", StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            if (IPAddress.TryParse(host, out IPAddress? ip))
+            {
+                return !IsPrivateIp(ip);
+            }
+
+            IPAddress[] addresses = await Dns.GetHostAddressesAsync(host);
+
+            foreach (IPAddress address in addresses)
+            {
+                if (IsPrivateIp(address))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private static bool IsPrivateIp(IPAddress ip)
+    {
+        byte[] bytes = ip.GetAddressBytes();
+
+        // IPv4
+        if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+        {
+            return bytes[0] switch
+            {
+                10 => true,
+                127 => true,
+                169 when bytes[1] == 254 => true,
+                172 when bytes[1] is >= 16 and <= 31 => true,
+                192 when bytes[1] == 168 => true,
+                _ => false,
+            };
+        }
+
+        // IPv6
+        if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+        {
+            return IPAddress.IsLoopback(ip) ||
+                   ip.IsIPv6LinkLocal ||
+                   ip.IsIPv6SiteLocal;
+        }
+
+        return false;
     }
 }
