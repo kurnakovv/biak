0ab2725688840866d786db5eeb11d29a3412c842
kurnakovv <maksk210@gmail.com>
2026-05-25 17:05:30 +0900

    More coverage for BiakApplicationException (#72)

--- src/Biak.ConsoleApp/Program.cs
+++ src/Biak.ConsoleApp/Program.cs
@@ -20,44 +20,37 @@ public static class Program
     /// <returns><placeholder>A <see cref="Task"/> representing the asynchronous operation.</placeholder></returns>
     public static async Task Main(string[] args)
     {
-        try
+        if (args.Length == 0)
         {
-            if (args.Length == 0)
-            {
-                Console.WriteLine(DocsConstant.GREETING);
-            }
-            else if (args.Length == 1 && args[0] == CommandArgumentConstant.HELP)
-            {
-                Console.WriteLine(DocsConstant.HELP);
-            }
-            else if (SetupCommand.IsRunnable(args))
-            {
-                await SetupCommand.RunAsync();
-            }
-            else if (DisableCommand.IsRunnable(args))
-            {
-                await DisableCommand.RunAsync();
-            }
-            else if (EnableCommand.IsRunnable(args))
-            {
-                await EnableCommand.RunAsync();
-            }
-            else if (FindActivityCommand.IsRunnable(args))
-            {
-                await FindActivityCommand.RunAsync();
-            }
-            else if (FindConflictsCommand.IsRunnable(args))
-            {
-                await FindConflictsCommand.RunAsync();
-            }
-            else
-            {
-                Console.WriteLine(UIConstant.NO_COMMAND);
-            }
+            Console.WriteLine(DocsConstant.GREETING);
         }
-        catch (BiakApplicationException)
+        else if (args.Length == 1 && args[0] == CommandArgumentConstant.HELP)
         {
-            throw;
+            Console.WriteLine(DocsConstant.HELP);
+        }
+        else if (SetupCommand.IsRunnable(args))
+        {
+            await SetupCommand.RunAsync();
+        }
+        else if (DisableCommand.IsRunnable(args))
+        {
+            await DisableCommand.RunAsync();
+        }
+        else if (EnableCommand.IsRunnable(args))
+        {
+            await EnableCommand.RunAsync();
+        }
+        else if (FindActivityCommand.IsRunnable(args))
+        {
+            await FindActivityCommand.RunAsync();
+        }
+        else if (FindConflictsCommand.IsRunnable(args))
+        {
+            await FindConflictsCommand.RunAsync();
+        }
+        else
+        {
+            Console.WriteLine(UIConstant.NO_COMMAND);
         }
     }
 }
