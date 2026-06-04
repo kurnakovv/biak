b91a4dd81cd96b9453f58d58e600d830ecab44c1
kurnakovv <maksk210@gmail.com>
2026-06-04 17:20:20 +0900

    Fix header for template project (#105)

--- tests/Biak.ConsoleApp.IntegrationTests/Templates/WebProjectWithWarnings/MyWebProjectTemplate/Program.cs
+++ tests/Biak.ConsoleApp.IntegrationTests/Templates/WebProjectWithWarnings/MyWebProjectTemplate/Program.cs
@@ -1,3 +1,7 @@
+// Copyright (c) 2026 kurnakovv
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for full license information.
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
