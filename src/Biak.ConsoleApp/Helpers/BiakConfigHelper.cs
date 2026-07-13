0c7bea4b08f976a2412e6680565c4d2cb7a88ad9
kurnakovv <maksk210@gmail.com>
2026-07-13 17:17:45 +0900

    Add new uncover code (#128)

--- src/Biak.ConsoleApp/Helpers/BiakConfigHelper.cs
+++ src/Biak.ConsoleApp/Helpers/BiakConfigHelper.cs
@@ -15,6 +15,16 @@ namespace Biak.ConsoleApp.Helpers;
 public static class BiakConfigHelper
 {
     /// <summary>
+    /// Check if .biak/config.json exists in the current directory.
+    /// </summary>
+    /// <returns>True when config exists; otherwise false.</returns>
+    public static bool ExistsInCurrentDirectory()
+    {
+        string configPath = Path.Join(".biak", "config.json");
+        return File.Exists(configPath);
+    }
+
+    /// <summary>
     /// Get .biak/config model.
     /// </summary>
     /// <param name="json">json content if you want to extract it not from '.biak/config.json' file.</param>
@@ -29,10 +39,6 @@ public static class BiakConfigHelper
                 return (BiakConfigConstant.FILE_NOT_FOUND, new BiakConfig());
             }
             json = await File.ReadAllTextAsync(configPath);
-        }
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            throw new NotImplementedException("Test codecov");
         }
         try
         {
