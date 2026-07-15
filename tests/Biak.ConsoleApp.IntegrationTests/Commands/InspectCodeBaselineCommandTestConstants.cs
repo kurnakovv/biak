72355c04eead46e0e24596a2c11c989d567b264d
kurnakovv <maksk210@gmail.com>
2026-07-15 21:21:07 +0900

    Add comment for every rule (#123)

--- tests/Biak.ConsoleApp.IntegrationTests/Commands/InspectCodeBaselineCommandTestConstants.cs
+++ tests/Biak.ConsoleApp.IntegrationTests/Commands/InspectCodeBaselineCommandTestConstants.cs
@@ -10,45 +10,59 @@ internal static class InspectCodeBaselineCommandTestConstants
 {
     public const string BASELINE_FILTERS = $$"""
         [{ServiceE.cs}]
+        # Use preferred body style (convert into property, indexer, or event with preferred body style) [ArrangeAccessorOwnerBody] | https://www.jetbrains.com/help/resharper/ArrangeAccessorOwnerBody.html
         resharper_arrange_accessor_owner_body_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceC.cs}]
+        # 'if' statement can be rewritten as '?:' expression [ConvertIfStatementToConditionalTernaryExpression] | https://www.jetbrains.com/help/resharper/ConvertIfStatementToConditionalTernaryExpression.html
         resharper_convert_if_statement_to_conditional_ternary_expression_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceE.cs}]
+        # Convert property into auto-property [ConvertToAutoProperty] | https://www.jetbrains.com/help/resharper/ConvertToAutoProperty.html
         resharper_convert_to_auto_property_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceA.cs}]
+        # Convert local variable or field into constant (private accessibility) [ConvertToConstant.Local] | https://www.jetbrains.com/help/resharper/ConvertToConstant.Local.html
         resharper_convert_to_constant_local_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceC.cs,ServiceE.cs}]
+        # Convert constructor into primary constructor [ConvertToPrimaryConstructor] | https://www.jetbrains.com/help/resharper/ConvertToPrimaryConstructor.html
         resharper_convert_to_primary_constructor_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceA.cs,ServiceC.cs}]
+        # Field can be made readonly (private accessibility) [FieldCanBeMadeReadOnly.Local] | https://www.jetbrains.com/help/resharper/FieldCanBeMadeReadOnly.Local.html
         resharper_field_can_be_made_read_only_local_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceB.cs}]
+        # Member can be made private (non-private accessibility) [MemberCanBePrivate.Global] | https://www.jetbrains.com/help/resharper/MemberCanBePrivate.Global.html
         resharper_member_can_be_private_global_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceB.cs}]
+        # Simplify negative equality expression [NegativeEqualityExpression] | https://www.jetbrains.com/help/resharper/NegativeEqualityExpression.html
         resharper_negative_equality_expression_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceE.cs}]
+        # Replace with single call to FirstOrDefault(..) [ReplaceWithSingleCallToFirstOrDefault] | https://www.jetbrains.com/help/resharper/ReplaceWithSingleCallToFirstOrDefault.html
         resharper_replace_with_single_call_to_first_or_default_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceD.cs}]
+        # Use 'String.IsNullOrEmpty' [ReplaceWithStringIsNullOrEmpty] | https://www.jetbrains.com/help/resharper/ReplaceWithStringIsNullOrEmpty.html
         resharper_replace_with_string_is_null_or_empty_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceA.cs,ServiceB.cs,ServiceC.cs,ServiceD.cs,ServiceE.cs}]
+        # Type member is never used (non-private accessibility) [UnusedMember.Global] | https://www.jetbrains.com/help/resharper/UnusedMember.Global.html
         resharper_unused_member_global_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceC.cs}]
+        # Type member is never used (private accessibility) [UnusedMember.Local] | https://www.jetbrains.com/help/resharper/UnusedMember.Local.html
         resharper_unused_member_local_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceA.cs,ServiceB.cs,ServiceC.cs,ServiceD.cs,ServiceE.cs}]
+        # Type is never used (non-private accessibility) [UnusedType.Global] | https://www.jetbrains.com/help/resharper/UnusedType.Global.html
         resharper_unused_type_global_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
 
         [{ServiceD.cs}]
+        # Use collection expression syntax [UseCollectionExpression] | https://www.jetbrains.com/help/resharper/UseCollectionExpression.html
         resharper_use_collection_expression_highlighting = suggestion {{InspectCodeBaselineInitCommandConstant.BASELINE_MARKER}}
         """;
 }
