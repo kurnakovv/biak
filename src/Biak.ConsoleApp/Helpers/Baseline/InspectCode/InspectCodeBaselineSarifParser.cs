39324b72734ec656d2a0baeb40a24c34c5566fc8
kurnakovv <maksk210@gmail.com>
2026-07-15 20:40:20 +0900

    get only properties (#123)

--- src/Biak.ConsoleApp/Helpers/Baseline/InspectCode/InspectCodeBaselineSarifParser.cs
+++ src/Biak.ConsoleApp/Helpers/Baseline/InspectCode/InspectCodeBaselineSarifParser.cs
@@ -2,7 +2,6 @@
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for full license information.
 
-using System.Linq;
 using System.Text.Json;
 using Biak.ConsoleApp.Models;
 
