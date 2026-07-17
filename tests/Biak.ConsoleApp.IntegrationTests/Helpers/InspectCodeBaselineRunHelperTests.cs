23719470f9652baa29624998a93f2aa425f76771
kurnakovv <maksk210@gmail.com>
2026-07-17 14:53:08 +0900

    Add integration test for if (errorOutput is not null) case (#123)

--- tests/Biak.ConsoleApp.IntegrationTests/Helpers/InspectCodeBaselineRunHelperTests.cs
+++ tests/Biak.ConsoleApp.IntegrationTests/Helpers/InspectCodeBaselineRunHelperTests.cs
@@ -12,6 +12,33 @@ namespace Biak.ConsoleApp.IntegrationTests.Helpers;
 public class InspectCodeBaselineRunHelperTests
 {
     [Fact]
+    public async Task RunAsyncWhenInspectCodeFailsShouldThrowBiakApplicationExceptionAsync()
+    {
+        TestDirectory testDir = new(
+            $"{nameof(InspectCodeBaselineRunHelperTests)}_{nameof(RunAsyncWhenInspectCodeFailsShouldThrowBiakApplicationExceptionAsync)}"
+        );
+
+        string templatePath = Path.Join(
+            AppContext.BaseDirectory,
+            "Templates",
+            "InspectCodeBaseline",
+            "InspectCodeBaselineTemplate"
+        );
+
+        testDir.CopyDirectory(templatePath);
+
+        string targetPath = Path.Join(testDir.Value, "InspectCodeBaselineTemplate.csproj");
+        await File.WriteAllTextAsync(targetPath, "this is not valid xml");
+
+        Exception? exception = await Record.ExceptionAsync(() =>
+            InspectCodeBaselineRunHelper.RunAsync(targetPath));
+
+        Assert.NotNull(exception);
+        Assert.IsType<BiakApplicationException>(exception);
+        Assert.StartsWith(InspectCodeBaselineRunHelperConstant.INSPECTCODE_FAILED, exception.Message, StringComparison.Ordinal);
+    }
+
+    [Fact]
     public async Task RunAsyncWhenInspectCodeIsNotAvailableShouldThrowBiakApplicationExceptionAsync()
     {
         TestDirectory testDir = new(
