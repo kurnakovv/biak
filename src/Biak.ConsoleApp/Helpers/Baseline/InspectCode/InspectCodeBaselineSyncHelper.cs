9538bcaface2578b21d0dc7a93b7d3d667e2126e
kurnakovv <maksk210@gmail.com>
2026-07-24 21:21:01 +0900

    Fix mappedRuleKey (#136)

--- src/Biak.ConsoleApp/Helpers/Baseline/InspectCode/InspectCodeBaselineSyncHelper.cs
+++ src/Biak.ConsoleApp/Helpers/Baseline/InspectCode/InspectCodeBaselineSyncHelper.cs
@@ -148,7 +148,7 @@ public static class InspectCodeBaselineSyncHelper
         }
 
         string ruleId = match.Groups["ruleId"].Value.Trim();
-        string? mappedRuleKey = mappedRuleKey = ruleIdOverrides is not null && ruleIdOverrides.TryGetValue(ruleId, out string? overrideRuleKey)
+        string? mappedRuleKey = ruleIdOverrides is not null && ruleIdOverrides.TryGetValue(ruleId, out string? overrideRuleKey)
             ? overrideRuleKey
             : InspectCodeRuleMetadataHelper.Get(ruleId)?.EditorconfigConfigKey;
 
